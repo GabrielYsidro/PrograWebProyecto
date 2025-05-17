fb37ce85bf66e16ab83d37cf16186bfbf04794fd
Gabriel Walter Ysidro Palomino <20222848@aloe.ulima.edu.pe>
2025-05-17 00:02:26 -0500

    feat: agregar pokebola al greeting

--- client/Front_PrograWeb/src/pages/PublicPages/Greeting.jsx
+++ client/Front_PrograWeb/src/pages/PublicPages/Greeting.jsx
@@ -1,10 +1,12 @@
 import {useState, useEffect, useContext} from 'react'
+import '../../styles/Greeting.module.css'
 
 
 export const Greeting = () => {
     return (
-        <div>
+        <div className='container'>
             Gracias por tu compra!
+            <img src='/src/assets/pokebola.gif' alt='Pokebola'></img>
         </div>
     )
 
