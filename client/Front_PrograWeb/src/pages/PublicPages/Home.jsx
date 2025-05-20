36190bbac8c54815cee1db71406ea321e9e3deb5
Gabriel Walter Ysidro Palomino <20222848@aloe.ulima.edu.pe>
2025-05-20 00:05:43 -0500

    fix: cambio directo en main de ciertas carpetas

--- client/Front_PrograWeb/src/pages/PublicPages/Home.jsx
+++ client/Front_PrograWeb/src/pages/PublicPages/Home.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { productos } from '../../constants/Consts.tsx';
-import TopBar from '../../extends/TopBar';
-import Footer from '../../extends/Footer'
+import { productos } from '../../constants/Consts.jsx';
+import TopBar from '../../components/TopBar/TopBar.jsx';
+import Footer from '../../components/Footer/Footer.jsx'
 
 export const Home = () => {
     const [busqueda, setBusqueda] = useState('');
