d9cd866120b73ed177b7f8f32a4131f8290aaee2
VaPsDani <20222771@aloe.ulima.edu.pe>
2025-05-24 21:25:56 -0500

    Fix: uso de context para carrito Feat: agregar producto al carrito

---
+++ client/Front_PrograWeb/src/hooks/GestorCategorias.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import {categorias} from "../constants/categorias";
+
+const GestorCategoriasContext = createContext();
+
+function useGestorCategorias() {
+    
+}
+
+export default useGestorCategorias;
