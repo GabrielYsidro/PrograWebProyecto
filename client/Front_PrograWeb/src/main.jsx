a57a0bb6d7674e40a9ac90303aa4bf0d9bd26dd7
VaPsDani <20222771@aloe.ulima.edu.pe>
2025-05-25 01:38:13 -0500

    Feat: Contexto de Ordenes

--- client/Front_PrograWeb/src/main.jsx
+++ client/Front_PrograWeb/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import AppRouter from './routes/AppRouter.jsx'
 import { CartProvider } from './hooks/CartContext.jsx';
 import { CategoriaProvider } from './hooks/CategoriaContext.jsx';
+import { OrdenContext } from './hooks/OrdenContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartProvider>
-      <CategoriaProvider>  
-        <AppRouter />
-      </CategoriaProvider> 
+      <OrdenContext>
+        <CategoriaProvider>  
+          <AppRouter />
+        </CategoriaProvider> 
+      </OrdenContext>
     </CartProvider>
   </StrictMode>,
 )
