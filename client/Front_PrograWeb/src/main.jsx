ec48231394f57aee4583f007d777f5665ada7a06
Gabriel Walter Ysidro Palomino <20222848@aloe.ulima.edu.pe>
2025-05-28 23:49:51 -0500

    fix: Ian

--- client/Front_PrograWeb/src/main.jsx
+++ client/Front_PrograWeb/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRouter from './routes/AppRouter.jsx'
-import { CartProvider } from './hooks/CartContext.jsx';
+import { CartProvider } from './contexts/CartContext.jsx';
 import { CategoriaProvider } from './hooks/CategoriaContext.jsx';
 import { OrdenProvider } from './hooks/OrdenContext.jsx';
 import { ProductosProvider } from './hooks/ProductosContext.jsx';
