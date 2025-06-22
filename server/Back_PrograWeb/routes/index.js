0c1cfed95c05ddb8024e8c620a4af0a806910eeb
Gabriel Walter Ysidro Palomino <20222848@aloe.ulima.edu.pe>
2025-06-22 12:20:31 -0500

    feat: conexion Db

--- server/Back_PrograWeb/routes/index.js
+++ server/Back_PrograWeb/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error en la base de datos');
+  }
 });
 
 module.exports = router;
