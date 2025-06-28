e0677703e646d9ac78b743e325cc273fb50b3bbb
Gabriel Walter Ysidro Palomino <20222848@aloe.ulima.edu.pe>
2025-06-28 18:29:05 -0500

    feat: controllers

--- server/Back_PrograWeb/routes/index.js
+++ server/Back_PrograWeb/routes/index.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+import { carritoControler} from '../controllers/carritoControler'
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/db', async (req, res) => {
-  try {
-    const users = await db.User.findAll(); 
-    res.json(users); 
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error en la base de datos');
-  }
-});
+router.get('/carrito', carritoControler() );
 
 router.get('/role', async (req, res) => {
   try {
