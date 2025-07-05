fa19cf7e1aeccfecd1c11b80a35bb4a70b865ccd
VaPsDani <20222771@aloe.ulima.edu.pe>
2025-07-05 16:19:11 -0500

    gddrgr

--- client/Front_PrograWeb/src/services/categoriaService.js
+++ client/Front_PrograWeb/src/services/categoriaService.js
@@ -2,6 +2,6 @@ const api = import.meta.env.VITE_API_URL;
 
 export async function getCategorias() {
   const res = await fetch(`${api}/categories`);
-  if (!res.ok) throw new Error('Error al obtener categorías');
+  if (!res.ok) throw new Error('Error al obtener categorías en servicio');
   return res.json();
 }
